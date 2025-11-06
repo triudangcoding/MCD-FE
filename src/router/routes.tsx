ebf249dabc5235a30241db43153373875c6965b6
auroraphtgrp01 <sliverknight4869@gmail.com>
2025-11-06 21:25:08 +0700

    refactor: remove App component and associated styles
    - Deleted the App component and its CSS styles to streamline the application structure.
    - Updated routing configuration to prepare for new components.

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,10 +1,12 @@
 import type { RouteObject } from 'react-router-dom';
 import RootLayout from '@/layouts/RootLayout';
+import Example from '@/pages/Example';
 export const routes: RouteObject[] = [
   {
     path: '/',
     element: <RootLayout />,
     children: [
+    
     ]
   }
 ];
