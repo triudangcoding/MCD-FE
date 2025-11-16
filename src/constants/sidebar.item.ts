585beb99fd8031a77e4a14af21fc72a4ba2b456d
auroraphtgrp01 <sliverknight4869@gmail.com>
2025-11-16 17:47:58 +0700

    refactor: clean up sidebar item constants by removing unused icons and menu items
    - Removed several unused icon imports from 'lucide-react' to streamline the code.
    - Eliminated the 'Order', 'Customer', 'Branch', 'Service', 'Department', 'Voucher', and 'Schedule' menu items from the sidebar, simplifying the menu structure.
    - Updated the menuItems array to reflect the changes, ensuring only relevant items are included.

--- src/constants/sidebar.item.ts
+++ src/constants/sidebar.item.ts
@@ -1,30 +1,11 @@
 import {
     LayoutDashboard,
     User,
-    Group,
-    Settings,
-    Key,
-    AlertTriangle,
-    Building,
-    ListOrdered,
-    Brush,
-    Contact,
-    Sparkles,
-    Ticket,
-    Calendar,
-    Calculator
 } from 'lucide-react';
 
 const menuItems = [
     { icon: LayoutDashboard, label: 'Dashboard', category: 'main', path: '/' },
-    { icon: ListOrdered, label: 'Order', category: 'main', path: '/orders' },
     { icon: User, label: 'Users', category: 'management', path: '/dashboard/users' },
-    { icon: Group, label: 'Customer', category: 'management', path: '/customer' },
-    { icon: Building, label: 'Branch', category: 'management', path: '/branch' },
-    { icon: Sparkles, label: 'Service', category: 'management', path: '/service' },
-    { icon: Brush, label: 'Department', category: 'management', path: '/department' },
-    { icon: Ticket, label: 'Voucher', category: 'management', path: '/voucher' },
-    { icon: Calendar, label: 'Schedule', category: 'management', path: '/schedule' },
 ];
 
 export default menuItems;
